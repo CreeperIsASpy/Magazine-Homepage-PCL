1ee5e1bf32cbc2a253f0df751a22cca370424ea3
CreeperIsASpy <162774918+CreeperIsASpy@users.noreply.github.com>
2025-04-20 17:46:33 +0800

    ahhhhhhh

--- reloader.py
+++ reloader.py
@@ -8,6 +8,8 @@ import requests
 from bs4 import BeautifulSoup
 
 headers = {
+    'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
     'Accept-Encoding': 'gzip, deflate, br',
     'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
